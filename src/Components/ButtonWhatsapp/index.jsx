0406ac8d5009143b7be3d8909dff0fbb5bbebb77
ivancho1392 <ivancho1392@gmail.com>
2023-06-12 01:00:30 -0500

    instagram button added

--- src/Components/ButtonWhatsapp/index.jsx
+++ src/Components/ButtonWhatsapp/index.jsx
@@ -5,15 +5,28 @@ import styles from "./styles.module.css";
 export const ButtonWhatsapp = () => {
   return (
     <div className="fixed bottom-12 left-8 p-3 rounded-lg cursor-pointer">
-      <Link className={styles.btnWhatsapp} href="http://wa.me/50763168396">
-        <Image
-          className={styles.btnWhatsappImage}
-          src="/whatsapp48.svg"
-          alt="Contact with Whatsapp"
-          width={50}
-          height={50}
-        />
-      </Link>
+      <div className='mb-4'>
+        <Link className={styles.btnWhatsapp} href="http://wa.me/50763168396">
+          <Image
+            className={styles.btnWhatsappImage}
+            src="/whatsapp48.svg"
+            alt="Contact with Whatsapp"
+            width={50}
+            height={50}
+          />
+        </Link>
+      </div>
+      <div>
+        <Link className={styles.btnInstagram} href="http://ig.me/m/ras_ivan1406">
+          <Image
+            className={styles.btnInstagramImage}
+            src="/Logos/Instagram.png"
+            alt="Contact with Instagram"
+            width={45}
+            height={45}
+          />
+        </Link>
+      </div>
     </div>
   );
 };
